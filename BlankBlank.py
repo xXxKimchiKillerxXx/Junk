4d25334af8730cd91231ea835a67ae849cdda714
xXxKimchiKillerxXx <hakjasu@gmail.com>
2023-09-03 09:34:05 +0900

    Update BlankBlank.py

--- BlankBlank.py
+++ BlankBlank.py
@@ -8,44 +8,52 @@ b = open('C:/Users/jsbjs/Desktop/코딩코/b.txt', 'r', encoding='UTF-8')
 a_lines = a.readlines()
 b_lines = b.readlines()
 
-print('\n')
 i = 0
 keywords = re.findall(r'\w+', b_lines[i])
 
 while i < len(a_lines):
     words = re.findall(r'\w+', a_lines[i])
     chars = re.findall(r'\W+', a_lines[i])
+
+    end = chars[-1]
+    escape = '. +'
+    p = re.compile(escape)
+    q = p.search(escape)
+    chars.remove(end)
+    chars.append(q.group())
+
+    print('\n\n')
+
     if re.match('\w', a_lines[i][0]):
         for j in range(len(words)):
             if words[j] in keywords:
                 print('___', end = '')
             else:
-                print(words[j], end='')
+                print(words[j], end = '')
             if j < len(chars):
-                print(chars[j], end='')
+                print(chars[j], end = '')
     else:
         for j in range(len(chars)):
-            print(chars[j], end='')
+            print(chars[j], end = '')
             if j < len(words):
                 if words[j] in keywords:
                     print('___', end = '')
                 else:
-                    print(words[j], end='')
+                    print(words[j], end = '')
 
-    print('\n')  # Print a newline to separate sentences
-    guess = input('Make a guess: ')
+    guess = input('\n\n\nMake a guess: ')
 
     if guess in keywords:
         keywords.remove(guess)
-        print('\n')
+        if len(keywords) == 0:
+            print('\n\n' + a_lines[i], end = '')
+            print(f'\n\n[Line {i + 1} of {len(a_lines)} complete]', end = '')
+            i += 1
+            if i < len(a_lines):
+                keywords = re.findall(r'\w+', b_lines[i])
     else:
-        print('Try again.')
-    if len(keywords) == 0:
-        print(a_lines[i] + '\n')
-        print(f'Sentence {i + 1} of {len(a_lines)} complete\n\n')
-        i += 1
-        if i < len(a_lines):
-            keywords = re.findall(r'\w+', b_lines[i])
+        print('\n\nTry again.', end = '')
+
 
 # Close the files
 a.close()
